eb1f6daf3ddb5c2f26997c30b7235ee8e3bfcf3e
Tamas LOVRANITS <tamas198601@gmail.com>
2025-02-17 20:00:48 +0100

    PTS-03-01 user side, questionnaire - get data in component

--- resources/js/components/pages/UQuestionnaireComponent.vue
+++ resources/js/components/pages/UQuestionnaireComponent.vue
@@ -1,22 +1,68 @@
 <template>
   <div class="p-2">
 
-    <h1 class="mb-5 mt-2 text-center">Kérdőív</h1>
+    <h1 class="mb-5 mt-2 text-center">Kérdőív kitöltése</h1>
 
     <div class="w-75 mx-auto">
+
+      <form
+          id="app"
+          ref="form"
+      >
+
+        <div v-if="pageLoad || (!pageLoad && !isBaseDataLoaded)" class="text-center alert alert-info fw-bold">
+          {{ message }}
+        </div>
+
+        <!-- messages -->
+        <div class="alert alert-success text-center font-weight-bold mt-3" role="alert" v-if="savingSuccessful">
+          Mentés megtörtént
+        </div>
+
+        <!-- modals -->
+        <b-modal ref="b-modal-form-error" ok-only centered title="Hiba" @ok="closeErrorModal()">
+          <ul class="my-4" v-if="errors.length">
+            <li v-for="error in errors">{{ error }}</li>
+          </ul>
+        </b-modal>
+
+        <div class="row">
+          <div class="col-12 col-md-6 mx-auto text-center">
+            <button
+                type="button"
+                @click.prevent="checkForm()"
+                class="btn btn-lg text-white"
+                :disabled="inProgress || emptyDatabase"
+            >Válaszok elküldése
+            </button>
+          </div>
+        </div>
+
+      </form>
+
     </div>
   </div>
 </template>
 
 <script>
-import {mapActions, mapGetters, mapMutations} from "vuex";
+import {mapActions, mapGetters, mapMutations, mapState} from "vuex";
 import {SET_SNACKBAR} from "../../store/constants";
 
 export default {
   name: "UQuestionnaireComponent",
 
   data() {
-    return {}
+    return {
+      message: 'Oldal töltődik',
+      pageLoad: true,
+      questionCollectionLoaded: false,
+      emptyDatabase: true,
+
+      inProgress: false,
+      savingSuccessful: false,
+
+      errors: []
+    }
   },
 
   created() {
@@ -24,6 +70,11 @@ export default {
   },
 
   computed: {
+    ...mapState({
+      isBaseDataLoaded() {
+        return this.questionCollectionLoaded;
+      },
+    }),
     ...mapGetters('Questionnaire', ["userQuestionCollection"])
   },
 
@@ -37,6 +88,30 @@ export default {
     });
   },
 
+  watch: {
+    userQuestionCollection: {
+      deep: true,
+      handler: function (newValue, oldValue) {
+        console.log('questionCollection változott:', oldValue, '->', newValue)
+
+        if (!this.userQuestionCollection || !this.userQuestionCollection.length) {
+          this.message = "A rendszer nem tartalmaz kérdőívet.";
+          this.questionCollectionLoaded = false;
+        } else {
+          this.questionCollectionLoaded = true;
+        }
+      }
+    },
+    isBaseDataLoaded(newValue) {
+      if (newValue) {
+        this.message = '';
+
+        this.pageLoad = false;
+        this.emptyDatabase = false;
+      }
+    },
+  },
+
   methods: {
     ...mapMutations({setSnackbar: SET_SNACKBAR}),
 
@@ -47,6 +122,41 @@ export default {
     },
 
     init() {
+    },
+
+    //save questions and answers
+    checkForm: function () {
+      //close snackbar;
+      this.setSnackbar({show: false, messages: []})
+
+      this.closeErrorModal();
+
+      this.errors = [];
+      this.savingSuccessful = false;
+
+      if (this.isInputsValid()) {
+        this.inProgress = true;
+
+      } else {
+        this.fillUpErrorArray();
+        return false;
+      }
+    },
+
+    isInputsValid() {
+    },
+
+    // --------- error modals ---------
+    fillUpErrorArray() {
+      this.showErrorModal();
+    },
+
+    showErrorModal() {
+      this.$refs['b-modal-form-error'].show();
+    },
+
+    closeErrorModal() {
+      this.$refs['b-modal-form-error'].hide();
     },
 
     // --------- pagination ---------
