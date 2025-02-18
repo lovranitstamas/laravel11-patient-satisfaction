b1d60061e846cf9eb3a00c31fc9578aa352d1021
Tamas LOVRANITS <tamas198601@gmail.com>
2025-02-18 21:09:49 +0100

    PTS-01-01 global settings, prefix css js routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
   <title>Beteg elégedettségi kérdőív</title>
 
   <!-- Scripts -->
-  <script src="{{ mix('js/app.js') }}" defer></script>
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 <body>
